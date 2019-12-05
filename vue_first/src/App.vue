72a2351aaf18962f2cb90ea4f16cc428b219c7d8
Cirike <374933269@qq.com>
2019-12-05 14:49:26 +0800

    init project

---
+++ vue_first/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div >
+    <div class="logo">
+      <img src="./assets/logo.png" alt="logo">
+    </div>
+    <div>
+      <HelloWorld/>
+    </div>
+  </div>
+
+</template>
+
+<script>
+    import HelloWorld from './component/HelloWorld';
+
+    export default {
+        components:{
+          HelloWorld
+        }
+    }
+</script>
+
+<style scoped>
+  .logo{
+    position: center;
+    height: 50px;
+    width: 50px;
+  }
+</style>
---
+++ vue_first/src/component/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="msg">
+      <p>{{msg}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+          return {
+            msg: "hi"
+          }
+        }
+
+
+    }
+</script>
+
+<style scoped>
+  .msg{
+    color: red;
+    heigh: 100 px;
+    withd: 100 px
+  }
+</style>
